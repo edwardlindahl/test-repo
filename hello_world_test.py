0c8e0ef52791cbd3e7de1600def93f8f7cd6147d
Edward Lindahl <edward.lindahl@gmail.com>
2020-11-26 10:52:43 -0500

    Pushing a new file to the repo.

---
+++ hello_world_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 10:49:51 2020
+
+@author: hp
+"""
+
+print("Hello world!")
+
+def plus_two(x):
+    return x + 2
+
+if __name__ == "__main__":
+    plus_two(20)
